8cee371c3feee8a148da0c40f387b10b3f847fc9
MMAlam23 <skmmalam1234@gmail.com>
2022-09-09 22:15:41 +0530

    1.0.0.1

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const errorhandler = require('./middleware/errorHandler')
 const router = require('./routes')
 require('dotenv').config()
 require('express-async-errors')
+const cors = require('cors')
 let ejs = require('ejs')
 const app = express()
 let port = process.env.PORT || 6000
 // uncaughtException Error Handler
-// process.on('uncaughtException', err => {
-//     console.log(`Error`, err)
-//     console.log("server Shut down due to uncaughtException")
-//     process.exit(1)
-// })
+process.on('uncaughtException', err => {
+    console.log(`Error`, err)
+    console.log("server Shut down due to uncaughtException")
+    process.exit(1)
+})
 
+app.use(cors())
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
@@ -29,10 +31,10 @@ app.use(errorhandler)
 let server = app.listen(port, () => console.log(`server is live ${port}!`))
 
 //  unhandled Promise Rejection Error handler 
-// process.on('unhandledRejection', err => {
-//     console.log(`error:${err.message}`)
-//     console.log('Server Shutdown due to unhandled Promise Rejection ')
-//     server.close(() => {
-//         process.exit(1)
-//     })
-// })
+process.on('unhandledRejection', err => {
+    console.log(`error:${err.message}`)
+    console.log('Server Shutdown due to unhandled Promise Rejection ')
+    server.close(() => {
+        process.exit(1)
+    })
+})
